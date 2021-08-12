3471404bbe214de367b48cdba696d41287134512
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-08-12 23:51:07 +0600

    Simple Pyramid

---
+++ simple-pyramid.js
@@ -0,0 +1,19 @@
+
+n = 5;
+
+for (var i = 1; i <= n; i++) {
+        result = ' ';
+        for (var j = 1; j <= i; j++) {
+                result += j + ' ';
+        }
+        console.log(result);
+}
+
+m = 2;
+for (var i = 1; i <= m; i++) {
+        result = ' ';
+        for (var j = 1; j <= m; j++) {
+                result += '* ';
+        }
+        console.log(result);
+}
